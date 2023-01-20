4bed470b132277e2c8a19e5d1b7d30978330a07f
Yanisvft <yanis.boudaoud24@gmail.com>
2023-01-20 12:01:53 +0100

    commit

---
+++ public/js/ScriptStreamEcran.js
@@ -0,0 +1,19 @@
+document.querySelector('#start-stream').addEventListener('click',async function captureScreen() {
+    try {
+        const stream = await navigator.mediaDevices.getDisplayMedia();
+        const video = document.querySelector('#video-stream');
+        video.srcObject = stream;
+        document.body.appendChild(video);
+        video.play();
+    } catch(err) {
+        console.error(err);
+    }
+})
+const socket = io("/");
+
+socket.emit('join-room',ROOM_ID,1);
+
+socket.on('user-connected',(userId)=>{
+    console.log(userId)
+});
+
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+
+app.set("view engine","ejs");
+app.use(express.static("public"), express.static("node_modules"));
+
+// app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
+
+app.get("/",(req,res)=>{
+    res.send('Hello app');
+})
+
+app.get("/:room",(req,res)=>{
+    //res.render("Room :" + {roomId: req.params.room});
+    res.render("room",{roomId: req.params.room});
+});
+
+io.on("connection",(socket) => {
+    socket.on("join-room", (roomId,userId)=>{
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected",userId);
+    });
+})
+
+server.listen("3000");
