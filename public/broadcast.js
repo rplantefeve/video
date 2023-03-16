a30917c6c6fc25e6e219c8b1ed565bb8608373d5
Yanisvft <yanis.boudaoud24@gmail.com>
2023-03-16 13:48:51 +0100

    Stream+fluxEnvoyer

---
+++ public/broadcast.js
@@ -0,0 +1,129 @@
+const peerConnections = {};
+const config = {
+  iceServers: [
+    { 
+      "urls": "stun:stun.l.google.com:19302",
+    },
+    // { 
+    //   "urls": "turn:TURN_IP?transport=tcp",
+    //   "username": "TURN_USERNAME",
+    //   "credential": "TURN_CREDENTIALS"
+    // }
+  ]
+};
+
+const socket = io.connect(window.location.origin);
+
+socket.on("answer", (id, description) => {
+  peerConnections[id].setRemoteDescription(description);
+  console.log("socket reçoit answer broadcast");
+});
+
+socket.on("watcher", id => {
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
+  console.log("socket reçoit watcher broadcast");
+  let stream = videoElement.srcObject;
+  stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+      console.log("socket envoie candidat broadcast");
+    }
+  };
+
+  peerConnection
+    .createOffer()
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      socket.emit("offer", id, peerConnection.localDescription);
+      console.log("socket envoie offer broadcast");
+    });
+});
+
+socket.on("candidate", (id, candidate) => {
+  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+  console.log("socket reçoit candidat broadcast");
+});
+
+socket.on("disconnectPeer", id => {
+  peerConnections[id].close();
+  delete peerConnections[id];
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+};
+
+// Get camera and microphone
+const videoElement = document.querySelector("video");
+const bouton = document.querySelector("button");
+//const audioSelect = document.querySelector("select#audioSource");
+//const videoSelect = document.querySelector("select#videoSource");
+
+//audioSelect.onchange = getStream;
+//videoSelect.onchange = getStream;
+bouton.addEventListener('click',getStream);
+//getStream();
+  //.then(getDevices);
+  //.then(gotDevices);
+
+function getDevices() {
+  return navigator.mediaDevices.enumerateDevices();
+}
+/*
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos;
+  for (const deviceInfo of deviceInfos) {
+    const option = document.createElement("option");
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === "audioinput") {
+      option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
+      audioSelect.appendChild(option);
+    } else if (deviceInfo.kind === "videoinput") {
+      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    }
+  }
+}
+*/
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  //const audioSource = audioSelect.value;
+  /*
+  const videoSource = videoSelect.value;
+  const constraints = {
+    //audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+    video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+  };
+  */
+  return navigator.mediaDevices
+    .getDisplayMedia(/*constraints*/)
+    .then(gotStream)
+    .catch(handleError);
+}
+
+function gotStream(stream) {
+  window.stream = stream;
+  /*
+  audioSelect.selectedIndex = [...audioSelect.options].findIndex(
+    option => option.text === stream.getAudioTracks()[0].label
+  );
+  
+  videoSelect.selectedIndex = [...videoSelect.options].findIndex(
+    option => option.text === stream.getVideoTracks()[0].label
+  );
+  */
+  videoElement.srcObject = stream;
+  socket.emit("broadcaster");
+  console.log("socket envoie  broadcast");
+}
+
+function handleError(error) {
+  console.error("Error: ", error);
+}
---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+let broadcaster;
+const port = 443;
+
+const options = {
+  cert: fs.readFileSync('certification/certificate.crt'),
+  key: fs.readFileSync('certification/private.key')
+  
+};
+
+const https = require("https");
+const server = https.createServer(options,app);
+
+const io = require("socket.io")(server);
+
+app.get("/", (req,res) => {
+  res.status(200).json({"message": "hw"})
+})
+app.use(express.static(__dirname + "/public"));
+try {
+  io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  const clientIpAddress = socket.handshake.address; //adresse ip du client
+  console.log(`Client connected with IP address ${clientIpAddress}`); 
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+
+  socket.on("ip", () => {
+    socket.to(id).emit("ip", clientIpAddress);
+  })
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
+  
+} catch (error) {
+  log(error.stack);
+
+}
+
+function log(message)
+{
+  // date = message
+  //console.log
+  // fs.write 
+}
